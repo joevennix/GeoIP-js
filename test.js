009879187855195dbfae35ef6c2a73f032576fc0
Joe Vennix <joevennix@gmail.com>
2011-03-30 14:38:30 -0500

    Added test.js for testing.

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 var geo = require('./geoip.js');
-geo.open({ cache: true, filename: './GeoLiteCity.dat'});
+
+geo.open({cache: true, filename: './GeoLiteCity.dat'});
 var coord = geo.lookup('74.125.227.16');
 console.log(coord);
 geo.close();
