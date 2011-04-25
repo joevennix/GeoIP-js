9a299a214c026a71d6ad7c1f4a1693cd9cc7ab1e
Joe Vennix <joevennix@gmail.com>
2011-04-25 02:08:26 -0500

    Fixed IP address issue in docs.

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 var geo = require('./geoip.js');
 
+// Open the database file and cache it
 geo.open({cache: true, filename: './GeoLiteCity.dat'});
+
+// Geocode an IP to a coordinate
 var coord = geo.lookup('74.125.227.16');
 console.log(coord);
+
+// Close the database file, delete cache
 geo.close();
